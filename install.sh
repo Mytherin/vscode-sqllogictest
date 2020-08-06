7d037122a6d0c89a0808fd5ede28207bfbac59c3
Mark Raasveldt <mark.raasveldt@gmail.com>
2020-08-06 21:29:41 +0200

    More grammar updates, highlight results as well now

---
+++ install.sh
@@ -0,0 +1,3 @@
+
+cp syntaxes/benchmark.tmLanguage.json ~/.vscode/extensions/benesch.sqllogictest-0.1.1/syntaxes/
+cp syntaxes/sqllogictest.tmLanguage.json ~/.vscode/extensions/benesch.sqllogictest-0.1.1/syntaxes/
